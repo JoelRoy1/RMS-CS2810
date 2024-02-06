4c2616a48607f145feb62ba65788535eb9683bb5
KEANU_TAMTEKIN <zlac384@rhul.ac.uk>
2024-02-06 16:30:48 +0000

    style: adjusted formatting and text of login container.

--- client/src/StaffLoginPage.js
+++ client/src/StaffLoginPage.js
@@ -6,7 +6,6 @@ function StaffLoginPage() {
   return (
     <div className="staff-login-page">
       <div className="login-container">
-        <h2 className="login-heading">Staff Login</h2>
         <form className="login-form">
           <label htmlFor="username" className="login-label">
             Staff ID
@@ -19,7 +18,7 @@ function StaffLoginPage() {
           <input type="password" id="password" className="login-input" />
 
           <button type="submit" className="login-button">
-            
+            Submit
           </button>
         </form>
       </div>
