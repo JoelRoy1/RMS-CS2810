eaab5b39551535e8bd9746855b384d2c336e18db
Joel Roy <[ZKAC408@live.rhul.ac.uk]>
2024-02-14 21:45:16 +0000

    fix: fix memory leak in order-controller.js
    Fixed a memory leak within try catch statement of order-controller.

--- server/controllers/order-controllers.js
+++ server/controllers/order-controllers.js
@@ -18,10 +18,11 @@ async function cancelOrder(orderId) {
     const client = await pool.connect();
     console.log('Connected to the database');
     await client.query('DELETE FROM orders WHERE order_id = $1', [orderId]);
-    client.release();
     console.log('Order cancelled successfully');
   } catch (error) {
     console.error(`Error cancelling order: ${error.message}`);
+  } finally {
+    client.release();
   }
 }
 
