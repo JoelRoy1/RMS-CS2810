3d00d7180dbbbaa1c1265745d6fd4fe09df6253d
Keanu Tamtekin <zlac384@live.rhul.ac.uk>
2024-02-15 00:58:41 +0000

    chore: set up menu index by path.

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MenuPage from './MenuPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const path = window.location.pathname;
+if (path === '/menu'){
+  root.render(
+    <React.StrictMode>
+      <MenuPage />
+    </React.StrictMode>
+  );
+
+} else {
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
