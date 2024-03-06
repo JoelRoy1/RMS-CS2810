071311d6d333be95d275977d8934af90bd1fae93
“Daniel <zlac236@live.rhul.ac.uk>
2024-03-06 19:28:01 +0000

    Feat: payment using stripe functional and tested

--- server/controllers/payment-controllers.js
+++ server/controllers/payment-controllers.js
@@ -9,31 +9,27 @@ const stripe = require('stripe')('sk_test_51OrP3NFH91vMUm0iDEYKaWF0bnVLTYPXHF9rF
 async function addPayment(amount, table_number, card_number, card_holder, card_expiry, card_cvc) {
     let client;
     try {
+        if (isNaN(amount)) {
+            throw new Error('Invalid amount');
+        }
         const stripePaymentIntent = await stripe.paymentIntents.create({
-            amount: amount,
-            currency: 'gdp',
-            payment_method_data: {
-                type: 'card',
-                card: {
-                    number: card_number,
-                    exp_month: card_expiry.split('/')[0],
-                    exp_year: card_expiry.split('/')[1],
-                    cvc: card_cvc,
-                    name: card_holder
-                }
-            },
-            confirm: true
+            amount: amount * 100,
+            currency: 'gbp',
+            payment_method_types: ['card'],
+            payment_method: 'pm_card_visa', //mock up card for stripe api
+            confirm: true,
+            description: `Payment for table ${table_number} by ${card_holder}`,
+            return_url: 'http://localhost:9000'
         })
-
-        if (stripePaymentIntent === 'succeeded') {
+        if (stripePaymentIntent.status === 'succeeded') {
             const client = await pool.connect();
-            const card_ending = card_number.split(-4); //store only last 4 digits of card number
+            const card_ending = card_number.substring(card_number.length - 4); //store only last 4 digits of card number
             const timestamp = new Date();
             const query = `INSERT INTO payments (payment_time, payment_amount, table_number, card_holder, card_ending, card_expiry)
             VALUES ($1, $2, $3, $4, $5, $6)`;
             const values = [timestamp, amount, table_number, card_holder, card_ending, card_expiry];
             await client.query(query, values);
-            return 'Payment made successully';
+            return 'Payment made successfully';
         }
     } catch (error) {
         console.error('Error processing payment', error);
