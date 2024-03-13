9f1a6d4d093e6e03a14599bdb2e8068237317043
“Zulfiqar <mohammad.ali.2020@live.rhul.ac.uk>
2024-03-13 22:24:18 +0000

    Refactor: StaffLoginPage.js
    implemented mui and cleaned up the page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LandingPage from './pages/LandingPage'
 import MenuPage from './pages/MenuPage'
 import MenuManagement from './pages/MenuManagement'
 import CustomerLoginPage from './pages/CustomerLoginPage'
+import StaffLoginPage from './pages/StaffLoginPage'
 import Dashboard from './pages/Dashboard'
 import './styles/App.css'
 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/menu-management" element={<MenuManagement />} />
           <Route path="/customer-login" element={<CustomerLoginPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/staff-login" element={<StaffLoginPage/>} />
           {/* Add more routes as needed */}
         </Routes>
         {/* Footer */}
