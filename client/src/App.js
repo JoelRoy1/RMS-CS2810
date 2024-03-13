f1e960713dc04ba56014b1e936c33b5903d06f5c
“Zulfiqar <mohammad.ali.2020@live.rhul.ac.uk>
2024-03-13 21:00:40 +0000

    feat: OmniNavbar.js
    Added a new navbar to navigate through the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import OmniNavbar from './components/OmniNavbar' // Assuming you've created this based on previous discussions
 import LandingPage from './pages/LandingPage'
-import StaffLoginPage from './pages/StaffLoginPage'
 import MenuPage from './pages/MenuPage'
 import MenuManagement from './pages/MenuManagement'
 import CustomerLoginPage from './pages/CustomerLoginPage'
 import Dashboard from './pages/Dashboard'
 import './styles/App.css'
 
-
 function App() {
-  const [showNav, setShowNav] = useState(false)
-
   return (
     <Router>
       <div className="App">
-        {/* Navigation bar */}
-        <div
-          className="App-nav-wrapper"
-          onMouseEnter={() => setShowNav(true)}
-          onMouseLeave={() => setShowNav(false)}
-        >
-          <div className="App-nav-trigger" />
-          <nav className={`App-nav ${showNav ? 'show' : ''}`}>
-            <Link to="/">Home</Link>
-            <Link to="/menu">Menu</Link>
-            <Link to="/about">About Us</Link>
-            <Link to="/menu-management">Menu Management</Link>
-            <Link to="/dashboard">Dashboard</Link>
-          </nav>
-        </div>
-
+        <OmniNavbar /> {/* Use your dedicated Navbar component here */}
         {/* Main content */}
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/menu" element={<MenuPage />} />
-          <Route path="/staff-login" element={<StaffLoginPage />} />
-          <Route path="/customer-login" element={<CustomerLoginPage />} />
           <Route path="/menu-management" element={<MenuManagement />} />
+          <Route path="/customer-login" element={<CustomerLoginPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          {/* Other routes here */}
+          {/* Add more routes as needed */}
         </Routes>
-
         {/* Footer */}
         <footer className="App-footer">
           <p>The Exquisite Oaxaca</p>
