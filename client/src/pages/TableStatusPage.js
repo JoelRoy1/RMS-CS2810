1df1b4d70d8c4ef723cae76ae500e86593ef2ab3
Aditya Raje <zlac230@live.rhul.ac.uk>
2024-03-25 18:38:48 +0000

    refactor: Made headers on table status page slightly smaller

--- client/src/pages/TableStatusPage.js
+++ client/src/pages/TableStatusPage.js
@@ -35,10 +35,10 @@ const TableStatusPage = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>Table Number</TableCell>
-              <TableCell style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>Customer Name</TableCell>
-              <TableCell style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>Order Status</TableCell>
-              <TableCell style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>Paid</TableCell>
+              <TableCell style={{ fontWeight: 'bold', fontSize: '1rem' }}>Table Number</TableCell>
+              <TableCell style={{ fontWeight: 'bold', fontSize: '1rem' }}>Customer Name</TableCell>
+              <TableCell style={{ fontWeight: 'bold', fontSize: '1rem' }}>Order Status</TableCell>
+              <TableCell style={{ fontWeight: 'bold', fontSize: '1rem' }}>Paid</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
